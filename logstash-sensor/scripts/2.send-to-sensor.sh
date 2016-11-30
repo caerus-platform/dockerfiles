903a0652df1216f256bbd2b1c815b767b3d454b1
Daniel Wei <danielwii0326@gmail.com>
2016-11-30 11:00:56 +0800

    update

--- logstash-sensor/scripts/2.send-to-sensor.sh
+++ logstash-sensor/scripts/2.send-to-sensor.sh
@@ -2,8 +2,13 @@
 
 export PATH=$PATH:/bin:/usr/local/bin:/usr/bin
 echo 'post logs to sensor...'
-while true; do
-    echo rsync -avz -e "ssh -p $REMOTE_PORT -i /opt/remote.key -o StrictHostKeyChecking=no" --progress --append /logstash-logs $REMOTE_URI:$REMOTE_PATH
-	rsync -avz -e "ssh -p $REMOTE_PORT -i /opt/remote.key -o StrictHostKeyChecking=no" --progress --append /logstash-logs/ $REMOTE_URI:$REMOTE_PATH
-	sleep 3
-done
+
+function start() {
+    while true; do
+        echo rsync -avz -e "ssh -p $REMOTE_PORT -i /opt/remote.key -o StrictHostKeyChecking=no" --progress --append /logstash-logs $REMOTE_URI:$REMOTE_PATH
+        rsync -avz -e "ssh -p $REMOTE_PORT -i /opt/remote.key -o StrictHostKeyChecking=no" --progress --append /logstash-logs/ $REMOTE_URI:$REMOTE_PATH
+        sleep 3
+    done
+}
+
+start &
